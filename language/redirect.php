9d9e7b99afccd2e9b1ea12ef1d9a4f7253b9b24e
s.ostrovskiy <s.ostrovskiy@pinta.com.ua>
2020-08-04 17:33:52 +0300

    first commit

---
+++ language/redirect.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']          = 'Redirect';
+
+// Text
+$_['text_success']           = 'Success: You changed the redirect!';
+$_['text_list']              = 'Redirects';
+$_['text_config']            = 'Editing Redirect';
+$_['text_add']               = 'Add Redirect';
+$_['text_edit']              = 'Edit Redirect';
+$_['text_default']           = 'Default';
+$_['text_extension']         = 'Extensions';
+$_['text_module_disabled']   = 'This module is disabled. To edit any redirect, you need to enable it';
+
+
+// Column
+$_['column_url_from']        = 'URL From';
+$_['column_url_to']          = 'URL To';
+$_['column_action']          = 'Actions';
+
+
+// Entry
+$_['entry_url_from']         = 'URL From';
+$_['entry_url_to']           = 'URL To';
+$_['entry_status']           = 'Status';
+
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the SEO URL is globally unique.';
+
+// Error
+$_['error_url_from_empty']       = 'The URL From is required.';
+$_['error_url_to_empty']         = 'The URL To is required.';
+$_['error_url_from_exists']      = 'This URL From was registrated already.';
+$_['error_url_to_dont_exist']    = 'This URL To don\'t exist.';
+$_['error_permission']           = 'Warning: You haven\'t permission to edit this redirect!';
